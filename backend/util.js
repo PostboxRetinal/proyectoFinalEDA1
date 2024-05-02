3d4b7ffaf542ff67411ace832d0f0d9cc9c04b22
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-05-02 16:26:12 -0500

    ca

--- backend/util.js
+++ backend/util.js
@@ -1,12 +1,12 @@
 function calcularScoreBoard(caso) {
     let penaltyTime = 0;
     if (caso no resuelto) {
-        
-    } else {
+       
         if (caso === "I") {
             penaltyTime += 20
         } 
-    }
+        
+    } 
     return caso;
 }
 
