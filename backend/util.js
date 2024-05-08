1f833be5a7783868e4055e1bc658cbdf7d9d447e
Nicolas Gonzalez Franco <nicolas290804@hotmail.com>
2024-05-08 01:20:04 -0500

    cambio

--- backend/util.js
+++ backend/util.js
@@ -1,13 +1,18 @@
 function calcularScoreBoard(caso) {
-    let penaltyTime = 0;
-    if (caso no resuelto) {
-       
-        if (caso === "I") {
-            penaltyTime += 20
-        } 
-        
-    } 
-    return caso;
+    let penaltyTime = 0;   
+    let casos = caso.split(";");
+    casos.forEach(submission => {
+
+        submission.split(" ");
+        let contest = submission [0];
+        let problem = submission [1];
+        let time = submission [2];
+        let L = submission [3];
+
+        if (L === "I") {
+            penaltyTime += 20;
+        }
+    });
 }
 
 export default calcularScoreBoard;
