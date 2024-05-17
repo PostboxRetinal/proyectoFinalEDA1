3d4a84708a7d27c22be510d6e9df9960ae10a48c
Andres Mauricio <andres123456jkil@gmail.com>
2024-05-17 16:02:15 -0500

    estilos provicionales

---
+++ frontend/style.cs
@@ -0,0 +1,20 @@
+table {
+    border: 2px;
+    background-color: aqua;
+    text-align: center;
+}
+
+fieldset{
+    width:500px;
+    height:360px;
+    border-radius:10px 10px 10px 10px;
+    background:#EEE8AA;
+    color:#000000;
+}
+
+
+
+
+
+
+
